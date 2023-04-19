47b45bc00d2a45fabea636c55951bfa8c26053d0
marcelo <marcellomello90{=@gmail.com>
2023-04-19 19:09:58 +0200

    first commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,82 @@
+const { hash, compare } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite")
+
+class UsersController{
+
+  //criando o usuario
+  async create(request, response){
+    const { name, email, password } = request.body;
+
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if(checkUserExists){
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [ name, email, hashedPassword ]
+    );
+
+    return response.status(201).json();
+  }
+
+
+  //atualizando um dado do usuario
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user){
+      throw new AppError("Usuário não encontrado")
+    }
+
+    //verificando se o email que ja existe
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    //verificando se o email que ja existe é de outro usuario
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    //verificando se o usuario digitou a senha antiga para alterar a senha
+    if( password && !old_password ) {
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
+    }
+
+    //verificar se a senha antiga digitada é realmente a senha cadastrada
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword){
+        throw new AppError("A senha antiga não confere.");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(`
+      UPDATE users SET 
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
+
+    return response.json()
+  }
+}
+
+module.exports = UsersController;
